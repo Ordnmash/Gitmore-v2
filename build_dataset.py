e303428cc8465bef0f3ba208ed0d4b6f769c8ba9
Ordnmash <ordnfxist@gmail.com>
2026-08-15 17:11:35 +0200

    Implement build_dataset function for commits

---
+++ build_dataset.py
@@ -0,0 +1,14 @@
+def build_dataset(commits):
+  x, y = [],[]
+  for c in commits:
+    xx = []
+    yy = []
+    for ch in '^' + c + '^':
+      xx.append(stoi[ch])
+      yy.append(stoi[ch])
+  
+    x.append(torch.tensor(xx[:-1]))
+    y.append(torch.tensor(yy[1:]))
+  
+  return x, y
+  
