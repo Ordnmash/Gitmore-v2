107658055977e179e91762d8a90ec8e46afde7bc
ordnmash <ordnfxist@gmail.com>
2026-08-27 23:44:38 +0200

    Fix indentation issue in build_dataset function

--- build_dataset.py
+++ build_dataset.py
@@ -2,7 +2,7 @@ def build_dataset(commits):
   x,y = [],[]
   for c in commits:
     xx,yy = [],[]
-    for ch in '^' + c + '^':
+    for ch in '^' + c + '^': 
       xx.append(stoi[ch])
       yy.append(stoi[ch])
     x.append(torch.tensor(xx[:-1]))
