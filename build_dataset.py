b944f47ee70054d88a99d378f8f632d4f699b1ee
ordnmash <ordnfxist@gmail.com>
2026-08-27 23:43:16 +0200

    Fix indentation and remove unnecessary newlines

--- build_dataset.py
+++ build_dataset.py
@@ -7,8 +7,6 @@ def build_dataset(commits):
     for ch in '^' + c + '^':
       xx.append(stoi[ch])
       yy.append(stoi[ch])
-  
     x.append(torch.tensor(xx[:-1]))
     y.append(torch.tensor(yy[1:]))
-    
   return x, y # return the dataset as list of variable sequence length
