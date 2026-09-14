049ba835de7c6830c9b816430b22b775e9008ea7
ordnmash <ordnfxist@gmail.com>
2026-09-14 22:12:12 +0200

    Fix formatting and add newline at end of file

--- build_dataset.py
+++ build_dataset.py
@@ -3,10 +3,11 @@ def build_dataset(commits):
   for c in commits:
     xx = [] 
     yy = []
-    for ic in '^' + c + '^': # wrap each commit between special tokens for training efficiency
+    for ic in '^' + c + '^':
       xx.append(stoi[ic])
       yy.append(stoi[ic])
       
     x.append(torch.tensor(xx[:-1]))
     y.append(torch.tensor(yy[1:]))
+  
   return (x,y)
